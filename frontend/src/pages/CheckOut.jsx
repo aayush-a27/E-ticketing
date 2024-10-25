7ff4f1ca4360d529b3ee90ec568cd5ae4cef19be
Aayush Bhadula <105729140+aayush-a27@users.noreply.github.com>
2024-10-25 17:54:56 +0530

    design checkOut

--- frontend/src/pages/CheckOut.jsx
+++ frontend/src/pages/CheckOut.jsx
@@ -1,34 +1,64 @@
 import { useLocation } from "react-router-dom";
 import { TiTick } from "react-icons/ti";
+
 const CheckOut = () => {
   const location = useLocation();
   const { seatSelected, selectedTime } = location.state;
 
   return (
-    <div className="border-2 border-black rounded-2xl mt-10 ml-32 pt-10 pl-32 gap-20 flex flexcol items-center justify-start">
-      <div>{selectedTime}</div>
-      <div className="">
-        <div className="text-4xl font-bold flex items-center justify-center py-2">Row</div>
-        {seatSelected.map((item, index) => (
-          <div key={index}>
-            <div className="font-medium py-2 items-center justify-center flex">{item.row}</div>
-          </div>
-        ))}
-      </div>
-      <div>
-        <div className="text-4xl font-bold flex items-center justify-center py-2 ">Seat Number</div>
-        {seatSelected.map((item, index) => (
-          <div key={index}>
-            <div className="items-center justify-center flex font-medium py-2">{item.seat}</div>
-          </div>
-        ))}
-      </div>
-        <div>
-        <div className="text-4xl font-bold flex items-center justify-center py-2">Selected</div>
-        <div>{seatSelected.map((items,keys)=>(
-            <div className="text-2xl items-center justify-center flex font-medium py-2" key = {keys}><TiTick /></div>
-        ))}</div>
+    <div className="box border-2 border-black rounded-2xl mt-10 ml-32 mr-32 pt-10 pl-32 gap-10 flex flex-col items-start justify-center relative">
+      {/* Bouncing ball */}
+      <div className="bouncing-ball"></div>
+
+      <div className=" flex items-center justify-center gap-10">
+        <div className="text text-4xl font-bold flex items-center justify-center py-2">
+          Show Timing
         </div>
+        <div className="text-2xl font-medium py-2 items-center justify-center flex">
+          {selectedTime}
+        </div>
+      </div>
+      <div className="flex items-center justify-start gap-20">
+        <div className="">
+          <div className="text-4xl font-bold flex items-center justify-center py-2">
+            Row
+          </div>
+          {seatSelected.map((item, index) => (
+            <div key={index}>
+              <div className="font-medium py-2 items-center justify-center flex">
+                {item.row}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div>
+          <div className="text-4xl font-bold flex items-center justify-center py-2">
+            Seat Number
+          </div>
+          {seatSelected.map((item, index) => (
+            <div key={index}>
+              <div className="items-center justify-center flex font-medium py-2">
+                {item.seat}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div>
+          <div className="text-4xl font-bold flex items-center justify-center py-2">
+            Selected
+          </div>
+          <div>
+            {seatSelected.map((items, keys) => (
+              <div
+                className="text-2xl items-center justify-center flex font-medium py-2"
+                key={keys}
+              >
+                <TiTick />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
