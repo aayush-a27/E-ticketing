5f6f3c4dccc986958acc8732dfd6ca9227796793
Aayush Bhadula <105729140+aayush-a27@users.noreply.github.com>
2024-12-22 19:01:34 +0530

    imporving UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,8 @@
 import { useNavigate } from 'react-router-dom';
+import cinemaImage1 from '../assets/cinema_image1-removebg-preview (1).png'
+import cinemaImage2 from  '../assets/cinema_image_2-removebg-preview.png'
+import  cinemaImage3 from '../assets/ccinema_image__3-removebg-preview.png'
+import cinemaImage4 from '../assets/cinemaImage4-removebg-preview.png'
 const Home = () => {
   const navigate = useNavigate();
   const handleButtonClick = () => {
@@ -6,6 +10,10 @@ const Home = () => {
   };
   return (
     <div className="h-[90%] w-full text-black">
+      <img className='absolute z-[-1] ' src={cinemaImage1} alt="" />
+      <img className=' absolute z-[-1] right-0' src={cinemaImage2} alt="" />
+      <img className='absolute z-[-1] scale-[0.6] left-[20%] py-44 bottom-0' src={cinemaImage3} alt="" />
+      <img className='absolute z-[-1] scale-[0.8] left-[55%] top-48 bottom-0' src={cinemaImage4} alt="" />
         <div className="py-24 w-full flex flex-col items-center justify-center">
           <h1 className="text-7xl">Wants to Book something?</h1>
           <button className='bg-green-500 py-3 px-4 rounded-xl mt-24 hover:shadow-xl hover:shadow-green-700/50' onClick={handleButtonClick}>Go to Book Page</button>
